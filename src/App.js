18a329b7913244fdde40d2a7d35c19e5da8d4e1e
Siegfred Samson <siegfredsamson@gmail.com>
2023-03-11 23:55:00 +0100

    Tailwind CSS initialized

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// import "./App.css";
 import { Navbar } from "./components/Navbar";
 
 function App() {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,13 @@ import { FiMenu } from "react-icons/fi";
 
 export const Navbar = () => {
   return (
-    <nav>
+    <nav className="w-16 h-16 bg-red-400">
       <p>Navbar</p>
       {/* Logo */}
       {/* User Sign in */}
       {/* Right Menu */}
       <FiMenu />
+      <h1 className="text-2xl font-bold underline">Hello world!</h1>
     </nav>
   );
 };
